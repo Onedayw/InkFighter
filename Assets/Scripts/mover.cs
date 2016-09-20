e87ed08eb4174c300176833b6a686beb3bac235a
Nate Wang <ytowang@ucdavis.edu>
2016-09-20 16:24:14 -0700

    Update mover target

--- Assets/Scripts/mover.cs
+++ Assets/Scripts/mover.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class mover : MonoBehaviour {
 
+	public GameObject target;
 	private Rigidbody2D rb2d;
 	public float speed;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
-		rb2d.velocity = new Vector3 (3, -3, 0) * speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (new Vector3 (0, 0, -450) * Time.deltaTime);
+		Transform targetTransform = target.transform;
+		rb2d.velocity = targetTransform.position * speed;
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
