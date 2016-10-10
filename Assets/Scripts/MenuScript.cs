c74485444976dfb44d3c85aa89fb64dd2304d0b8
Yuki <weihan@usc.edu>
2016-10-10 16:16:32 -0700

    add a start scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public void onStartGame()
+	{
+		Debug.Log ("start");
+		SceneManager.LoadScene ("main_g");
+	}
+
+}
