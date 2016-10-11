d78aada2c059dd1996d5d90e6c34fd0fd3981e48
ZIAO XU <oliverxu1123@gmail.com>
2016-10-10 23:45:06 -0700

    Two secens add UI

---
+++ Assets/Scripts/Button UI Scripts/tutorail.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class tutorail : MonoBehaviour {
+	
+	bool mouseOver = false;
+	public Button tutorailButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter() {
+		mouseOver = true;
+		tutorailButton.image.enabled = false;
+	}
+}
