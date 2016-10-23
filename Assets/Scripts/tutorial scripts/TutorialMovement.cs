25988ee6fbe9ba609a22736f8a5cf4e7fb647f37
ZIAO XU <oliverxu1123@gmail.com>
2016-10-23 01:06:21 -0700

    tutorail update v1.2
    add movie to tutorail

--- Assets/Scripts/tutorial scripts/TutorialMovement.cs
+++ Assets/Scripts/tutorial scripts/TutorialMovement.cs
@@ -5,29 +5,21 @@ using UnityEngine.SceneManagement;
 public class TutorialMovement : MonoBehaviour {
 	public GameObject player;
 	private Vector3 origin;
-	bool flag;
-	private float firstTime;
 
 	// Use this for initialization
 	void Start () {
 		origin = player.transform.position;
-		flag = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (player.transform.position != origin) {
-			if (flag) {
-				firstTime = Time.time;
-				flag = false;
-			}
-			toNextScene ();
+			StartCoroutine(toNextScene());
 		}
 	}
 
-	void toNextScene() {		
-		if (Time.time - firstTime >= 3f) {
-			SceneManager.LoadScene ("tutorial_kill");
-		}
+	IEnumerator toNextScene() {		
+		yield return new WaitForSeconds(5);
+		SceneManager.LoadScene ("tutorial_kill");
 	}
 }
--- Assets/Scripts/tutorial scripts/movement_vedio.cs
+++ Assets/Scripts/tutorial scripts/movement_vedio.cs
@@ -2,18 +2,25 @@
 using System.Collections;
 using UnityEngine.UI;
 
+
 public class movement_vedio : MonoBehaviour {
 
 	public MovieTexture movie;
+	public RawImage rawImage;
 
 	// Use this for initialization
 	void Start () {
 		GetComponent<RawImage> ().texture = movie as MovieTexture;
 		movie.Play ();
+		StartCoroutine(endMovie());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	IEnumerator endMovie() {		
+		yield return new WaitForSeconds(5);
+		Destroy (rawImage);
 	}
 }
