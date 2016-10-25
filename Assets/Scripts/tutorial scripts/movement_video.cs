d2bcca08d04f023ac81ea64e3fb5848757ffc9d5
Nate Wang <ytowang@ucdavis.edu>
2016-10-25 00:50:30 -0700

    Update tutorial clips

---
+++ Assets/Scripts/tutorial scripts/movement_video.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class movement_video : MonoBehaviour {
+
+	public MovieTexture movie;
+	public RawImage rawImage;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		movie.Play ();
+		StartCoroutine(endMovie());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	IEnumerator endMovie() {		
+		yield return new WaitForSeconds(5);
+		Destroy (rawImage);
+	}
+}
