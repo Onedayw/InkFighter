d2e479d8db8cf0c0a4f41a0737c1010b701ae3aa
Managa85 <Mana@guest-wireless-upc-1607-10-120-117-215.usc.edu>
2016-11-15 15:13:15 -0800

    audio

---
+++ Assets/Scripts/AudioForBoss.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioForBoss : MonoBehaviour {
+
+	//public AudioClip shootSound;
+	public AudioSource source;
+	public Camera cam;
+	public AudioSource MainBGM;
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+		cam = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 screenPoint = cam.WorldToViewportPoint(this.transform.position);
+		Debug.Log (screenPoint.x+" "+screenPoint.y+" "+screenPoint.z);
+		bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+		//bool onScreen =renderer.isVisible;
+		if (onScreen&&!source.isPlaying) {
+			//Debug.Log ("IM IN");
+			MainBGM.Stop();
+			source.Play();
+		}
+	}
+}
